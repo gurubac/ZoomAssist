04d0561327f1b5f8b2f94f187e477ad3bd2a4c9c
gurubac <gbrbachchu@gmail.com>
2021-03-24 18:01:22 -0700

    working version

--- main.py
+++ main.py
@@ -5,11 +5,32 @@ from dotenv import load_dotenv
 load_dotenv()
 #load_dotenv('---.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
 
 client = discord.Client()
 #print(TOKEN)
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+@client.event
+async def on_message(message):
+    id = client.get_guild(GUILD)
+
+
+    if message.content.find("!hello") != -1:
+        await message.channel.send("Hi")
+    elif message.content == ("!stop"):
+        await client.close()
+
+
 
 client.run(TOKEN)
