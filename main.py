0f163eb71ec7fc19985fc0d5b8b148f2c227d9dd
Calvin Vu <vucalvin95@gmail.com>
2021-03-25 16:38:00 -0700

    updated calvin branch

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ client = discord.Client()
 #print(TOKEN)
 @client.event
 async def on_ready():
+    print(f'Bot is Online.')
     for guild in client.guilds:
         if guild.name == GUILD:
             break
@@ -26,13 +27,18 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    id = client.get_guild(GUILD)
-
-
-    if message.content.find("!hello") != -1:
+    #id = client.get_guild(GUILD)
+    if message.content.startswith("!hello") != -1:
         await message.channel.send("Hi")
+    elif message.content.startswith('!zoom m'):
+        #if there are no meetings at specific time
+            await message.channel.send(f'There are currently no Zoom meetings.')
+        #if there are meetings at specific time
+    elif message.content.startswith('!zoom s'):
+        await message.channel.send(f'Your scheduled Zoom meeting is on ')
     elif message.content == ("!stop"):
         await client.close()
+
 
 
 
