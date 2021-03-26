1f9121afd2cd7c13c41a6bd5df347f0685d66bfb
gurubac <gbrbachchu@gmail.com>
2021-03-26 15:25:56 -0700

    schedule text database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import tasks, commands
 from dotenv import load_dotenv
 import subprocess
 import pyautogui
@@ -9,10 +10,9 @@ from datetime import datetime
 import requests
 import json
 import http.client
+import asyncio
 
-
-#new comment
-#another comment
+#bot = commands.Bot(command_prefix=".")
 
 load_dotenv()
 #load_dotenv('---.env')
@@ -20,6 +20,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 AUTH = os.getenv('AUTHORIZATION')
 #DISCORDCHANNEL = os.getenv('DISCORD_CHANNEL')
+
+isMeetingLive = None
 
 def myInfo():
     conn = http.client.HTTPSConnection("api.zoom.us")
@@ -43,6 +45,7 @@ def myInfo():
 
 
 def createMeeting():
+    isMeetingLive = True
     conn = http.client.HTTPSConnection("api.zoom.us")
 
     payload = "{\"topic\":\"string\",\"type\":\"integer\",\"start_time\":\"string [date-time]\",\"duration\":\"integer\",\"schedule_for\":\"string\",\"timezone\":\"string\",\"password\":\"string\",\"agenda\":\"string\",\"recurrence\":{\"type\":\"integer\",\"repeat_interval\":\"integer\",\"weekly_days\":\"string\",\"monthly_day\":\"integer\",\"monthly_week\":\"integer\",\"monthly_week_day\":\"integer\",\"end_times\":\"integer\",\"end_date_time\":\"string [date-time]\"},\"settings\":{\"host_video\":\"boolean\",\"participant_video\":\"boolean\",\"cn_meeting\":\"boolean\",\"in_meeting\":\"boolean\",\"join_before_host\":\"boolean\",\"mute_upon_entry\":\"boolean\",\"watermark\":\"boolean\",\"use_pmi\":\"boolean\",\"approval_type\":\"integer\",\"registration_type\":\"integer\",\"audio\":\"string\",\"auto_recording\":\"string\",\"enforce_login\":\"boolean\",\"enforce_login_domains\":\"string\",\"alternative_hosts\":\"string\",\"global_dial_in_countries\":[\"string\"],\"registrants_email_notification\":\"boolean\"}}"
@@ -61,18 +64,56 @@ def createMeeting():
     print(data.decode("utf-8"))
 
 #createMeeting()
+
+
+    
+
 myInfo()
 
 #discord getting info
-client = discord.Client()
+#client = discord.Client()
+#bot = discord.Client()
+bot = commands.Bot(command_prefix=".")
 #channel = client.get_channel(DISCORDCHANNEL)
-id = client.get_guild(GUILD)
+id = bot.get_guild(GUILD)
 #print(channel)
 #print(id)
 
-@client.event
+@bot.command(
+    aliases=["setschedule", "sets", "changeschedule", "changesched", "setSchedule", "setS"]
+)
+async def echo(ctx):
+    await ctx.message.delete()
+    embed = discord.Embed(
+        title="Set the schedule for your Zoom meetings!\nMake sure to type in all courses and timeslots!\nEx: Math1A: MTWRF 9am-1030am, Math1B: TR 5pm-7pm",
+        description="This request will time out in 1 minute!",
+    )
+    sent = await ctx.send(embed=embed)
+
+    try:
+        msg = await bot.wait_for(
+            "message",
+            timeout=59,
+            check= lambda message: message.author == ctx.author and message.channel == ctx.channel
+        ) 
+        if msg:
+            await sent.delete()
+            await msg.delete()
+            await ctx.send(msg.content)
+            global sched 
+            sched = str(msg.content)
+            f = open('schedule.txt', 'w')
+            f.write(sched)
+            f.close()
+            return sched
+    except asyncio.TimeoutError:
+        await sent.delete()
+        await ctx.send("Cancelling due to timeout.", delete_after=15)
+
+
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if guild.name == GUILD:
             break
 
@@ -92,14 +133,22 @@ collection = myInfo()
 print(collection)
 print(type(collection))
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.content.find("!new") != -1:
+    await bot.process_commands(message)
+    if message.content.find("!meeting") != -1:
         await message.channel.send("Hello " + collection[1] + " " + collection[2] + ", here is your zoom link created at " + collection[3] + "!")
         time.sleep(2)
         await message.channel.send(collection[0])
     elif message.content == ("!stop"):
-        await client.close()
-        
+        await bot.close()
+    elif message.content.startswith("!zoom s"):
+        f = open('schedule.txt', 'r')
+        await message.channel.send(f"Your scheduled Zoom meetings are on: " + f.read())
 
-client.run(TOKEN)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run(TOKEN)
