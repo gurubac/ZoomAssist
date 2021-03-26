8fb01c27b090cac3cd1885f6c6b5e5c529eefe73
Calvin Vu <vucalvin95@gmail.com>
2021-03-26 16:44:07 -0700

    updated latest

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ def myInfo():
 
 
 def createMeeting():
-    isMeetingLive = True
     conn = http.client.HTTPSConnection("api.zoom.us")
 
     payload = "{\"topic\":\"string\",\"type\":\"integer\",\"start_time\":\"string [date-time]\",\"duration\":\"integer\",\"schedule_for\":\"string\",\"timezone\":\"string\",\"password\":\"string\",\"agenda\":\"string\",\"recurrence\":{\"type\":\"integer\",\"repeat_interval\":\"integer\",\"weekly_days\":\"string\",\"monthly_day\":\"integer\",\"monthly_week\":\"integer\",\"monthly_week_day\":\"integer\",\"end_times\":\"integer\",\"end_date_time\":\"string [date-time]\"},\"settings\":{\"host_video\":\"boolean\",\"participant_video\":\"boolean\",\"cn_meeting\":\"boolean\",\"in_meeting\":\"boolean\",\"join_before_host\":\"boolean\",\"mute_upon_entry\":\"boolean\",\"watermark\":\"boolean\",\"use_pmi\":\"boolean\",\"approval_type\":\"integer\",\"registration_type\":\"integer\",\"audio\":\"string\",\"auto_recording\":\"string\",\"enforce_login\":\"boolean\",\"enforce_login_domains\":\"string\",\"alternative_hosts\":\"string\",\"global_dial_in_countries\":[\"string\"],\"registrants_email_notification\":\"boolean\"}}"
@@ -64,9 +63,6 @@ def createMeeting():
     print(data.decode("utf-8"))
 
 #createMeeting()
-
-
-    
 
 myInfo()
 
@@ -151,4 +147,4 @@ async def on_message(message):
 async def ping(ctx):
     await ctx.send('Pong!')
 
-bot.run(TOKEN)
+bot.run(TOKEN)                                                                                                     
